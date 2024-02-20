86b99ea6de3227b1c6641e3c64b34f1b43c39996
Russell <rkay@opera.com>
2024-02-20 16:10:59 +0000

    Updated changes for 2024.2

--- Tools/createGMLSpecReleaseNotes.bat
+++ Tools/createGMLSpecReleaseNotes.bat
@@ -1,5 +1,5 @@
 set LOCALGM=c:/source/GameMaker
-set RUNTIMEGM=C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.8.2.152
+set RUNTIMEGM=C:\ProgramData\GameMakerStudio2\Cache\runtimes\runtime-2023.11.1.160
 python "compareGMLSpec.py"  "%LOCALGM%/Zeus/compiler/GMLSpec.xml" "%RUNTIMEGM%/GmlSpec.xml" >c:\temp\output.md
 python "compareGmlSpec.py"  "%LOCALGM%/Zeus/compiler/PS4/GMLSpec.xml" "%RUNTIMEGM%/PS4/GmlSpec.xml" >>c:\temp\output.md
 python "compareGmlSpec.py"  "%LOCALGM%/Zeus/compiler/PS5/GMLSpec.xml" "%RUNTIMEGM%/PS5/GmlSpec.xml" >>c:\temp\output.md
